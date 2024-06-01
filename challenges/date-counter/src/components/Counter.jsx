bbfdf86c099fdb24a52869f557ec8d53280f3251
MI-Progammer <ilhamdivine7@gmail.com>
2024-06-01 09:29:24 +0700

    Completed the challange #2: Date Counter V2 in section 6 of the course

--- challenges/date-counter/src/components/Counter.jsx
+++ challenges/date-counter/src/components/Counter.jsx
@@ -7,23 +7,41 @@ const Counter = () => {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
-  const decreaseStep = () => setStep((s) => (s > 1 ? s - 1 : s));
-  const increaseStep = () => setStep((s) => s + 1);
+  // const decreaseStep = () => setStep((s) => (s > 1 ? s - 1 : s));
+  // const increaseStep = () => setStep((s) => s + 1);
 
   const decreaseCount = () => setCount((c) => c - step);
   const increaseCount = () => setCount((c) => c + step);
 
+  const handleResetState = () => {
+    setCount(0);
+    setStep(1);
+  };
+
   return (
     <div>
       <div className="step">
-        <button onClick={decreaseStep}>-</button>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span style={{ marginLeft: "10px" }}>Step : {step}</span>
+        {/* <button onClick={decreaseStep}>-</button>
         <span>Step : {step}</span>
-        <button onClick={increaseStep}>-</button>
+        <button onClick={increaseStep}>-</button> */}
       </div>
 
       <div className="count">
         <button onClick={decreaseCount}>-</button>
-        <span>Count : {count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        {/* <span>Count : {count}</span> */}
         <button onClick={increaseCount}>+</button>
       </div>
 
@@ -37,6 +55,13 @@ const Counter = () => {
             : " days ago was "}
         </span>
         <span>{date.toDateString()}</span>
+        <div>
+          {step !== 1 || count !== 0 ? (
+            <button onClick={handleResetState}>Reset</button>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
