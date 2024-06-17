b129d176bece600a8da5dec53cd4a726cc3eb0fc
MI-Progammer <ilhamdivine7@gmail.com>
2024-06-17 22:12:59 +0700

    Completed exercise #15: The world bank reducer in section 20 of the course

---
+++ 15-redux-intro/starter/src/features/accounts/BalanceDisplay.jsx
@@ -0,0 +1,27 @@
+import { useSelector, connect } from "react-redux";
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+// function BalanceDisplay() {
+//   const { balance } = useSelector((store) => store.account);
+
+//   return <div className="balance">{formatCurrency(balance)}</div>;
+// }
+
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
+}
+
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+// export default BalanceDisplay;
+export default connect(mapStateToProps)(BalanceDisplay);
---
+++ 15-redux-intro/starter/src/features/customers/customerSlice-v1.js
@@ -0,0 +1,36 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const createCustomer = (fullName, nationalID) => ({
+  type: "customer/createCustomer",
+  payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+});
+
+export const updateName = (fullName) => ({
+  type: "customer/updateName",
+  payload: fullName,
+});
